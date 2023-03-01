63ddbc02df5207a568e8f52062901ab20ef762e3
kzsy <kzamani44@gmail.com>
2023-03-01 17:17:45 +0100

    created number list

--- js/app.js
+++ js/app.js
@@ -18,26 +18,26 @@
 // divCon.style.margin = '30px auto'
 // divCon.style.textAlign = 'center'
 
-// let nums
+// let li
 // for (let i = 0; i <= 101; i++) {
-//     nums = document.createElement('h2')
-//     nums.className = 'nos'
-//     // nums.style.height = '50px'
-//     nums.style.width = '80px'
-//     // nums.style.paddingLeft = '30px'
-//     // nums.style.paddingRight = '30px'
-//     nums.style.paddingTop = '10px'
-//     nums.style.paddingBottom = '10px'
-//     // nums.style.margin = '0 auto'
-//     nums.style.color = "#fff"
-//     nums.style.backgroundColor = "#FD5E53"
-//     nums.textContent = i
-//     nums.setAttribute('id',i)
+//     li = document.createElement('h2')
+//     li.className = 'nos'
+//     // li.style.height = '50px'
+//     li.style.width = '80px'
+//     // li.style.paddingLeft = '30px'
+//     // li.style.paddingRight = '30px'
+//     li.style.paddingTop = '10px'
+//     li.style.paddingBottom = '10px'
+//     // li.style.margin = '0 auto'
+//     li.style.color = "#fff"
+//     li.style.backgroundColor = "#FD5E53"
+//     li.textContent = i
+//     li.setAttribute('id',i)
 
-//     if(parseInt(nums.textContent) % 2 === 0){
-//         nums.style.backgroundColor = "#21BF73"
+//     if(parseInt(li.textContent) % 2 === 0){
+//         li.style.backgroundColor = "#21BF73"
 //     }else{
-//         nums.style.backgroundColor = "#FDDB3A" 
+//         li.style.backgroundColor = "#FDDB3A" 
 //     }
 //     function isPrime(n){
 //         if(n<=1){
@@ -53,10 +53,10 @@
 //         }
 //         return true
 //     }
-//     if(isPrime(parseInt(nums.textContent)) === true){
-//         nums.style.backgroundColor = '#FD5E53'
+//     if(isPrime(parseInt(li.textContent)) === true){
+//         li.style.backgroundColor = '#FD5E53'
 //     }
-//     divCon.appendChild(nums)
+//     divCon.appendChild(li)
 // }
 const div = () => {
     let divCon = document.querySelector('.container')
@@ -80,11 +80,10 @@ const header = () => {
     let title = document.createElement('h1')
     title.className = 'title'
     title.textContent = "Number Generator"
-    // divCon.appendChild(title)
-    // console.log(title)
-    return title
+
+    div().appendChild(title)
 }
-div().appendChild(header())
+
 
 const subHeader = () => {
     let subTitle = document.createElement('h3');
@@ -103,4 +102,58 @@ const subHeader = () => {
     div().appendChild(subTitle)
     div().appendChild(author)
 }
+
+const numberGenerator = () => {
+    let ul = document.createElement('ul')
+    ul.className = 'numContainer'
+    ul.style.width = '500px'
+    ul.style.height = 'fit-content'
+    ul.style.display = 'flex'
+    ul.style.flexWrap = 'wrap'
+    ul.style.gap = '3px'
+    ul.style.margin = '20px auto'
+    // ul.style.backgroundColor = '#121212'
+    let li
+    for(let i = 0; i <= 101; i++) {
+        li = document.createElement('li')
+        li.className = 'nos'
+        li.style.listStyle = 'none'
+        li.style.width = '80px'
+        li.style.paddingTop = '10px'
+        li.style.paddingBottom = '10px'
+        li.style.color = "#fff"
+        li.style.backgroundColor = "#FD5E53"
+        li.textContent = i
+        li.setAttribute('id',i)
+
+        if(parseInt(li.textContent) % 2 === 0){
+            li.style.backgroundColor = "#21BF73"
+        }else{
+            li.style.backgroundColor = "#FDDB3A" 
+        }
+        function isPrime(n){
+            if(n<=1){
+                return false
+            }else if(n === 2){
+                return true
+            }
+
+            for (let i = 2; i < n; i++) {
+                if(n % i == 0){
+                    return false
+                }
+            }
+            return true
+        }
+        if(isPrime(parseInt(li.textContent)) === true){
+            li.style.backgroundColor = '#FD5E53'
+        }
+        
+        ul.appendChild(li)
+    }
+    div().appendChild(ul)
+}
+
+header()
 subHeader()
+numberGenerator()
